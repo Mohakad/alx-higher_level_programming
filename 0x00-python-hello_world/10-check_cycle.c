8fe6ea260a904278007c399cfaa782b702e8cc5f
Mohakad <mohkadtrd@gmail.com>
2023-11-29 18:02:21 +0300

    cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,4 +6,18 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *onenext = list;
+
+	listint_t *twonext = list;
+
+	if (!list)
+		return (0);
+	while (onenext && twonext && twonext->next)
+	{
+		onenext = onenext->next;
+		twonext = twonext->next->next;
+		if (onenext == twonext) /*If list has a loop they will definitely meet*/
+			return (1);
+	}
+	return (0);
 }
