11b3ab10947ece462ac19f0617eea8618fbe8aef
Mohakad <mohkadtrd@gmail.com>
2023-12-18 06:29:11 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    leng = 0;
+    leng = 0
     for ind in range(x):
         try:
             print(my_list[ind], end="")
@@ -9,4 +9,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return leng
-
