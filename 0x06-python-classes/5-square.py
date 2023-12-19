fdc830fb6d7227890ed02cf4d061fd4090810039
Mohakad <mohkadtrd@gmail.com>
2023-12-19 19:45:52 +0300

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size):
+        """Initializ square
+        Args:
+            size (int): size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
