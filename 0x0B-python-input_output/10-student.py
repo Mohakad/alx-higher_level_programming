ddc41b324431613f0becc8c3de22668a399cb001
Mohakad <mohkadtrd@gmail.com>
2024-01-09 21:20:29 +0300

    crr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student."""
+
+
+class Student:
+    """student"""
+
+    def __init__(self, first_name, last_name, age):
+        """new Student.
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict
+
+        Args:
+            attrs (list): attribute
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
