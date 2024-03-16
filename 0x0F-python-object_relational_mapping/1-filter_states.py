f54d7d0fa3740d8f1fbd568bcabc43b145cf21ef
Mohakad <mohkadtrd@gmail.com>
2024-03-16 17:51:10 +0300

    1flt

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""starting with N (upper N) from the database"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    dtb = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                          passw=sys.argv[2], dtb=sys.argv[3], port=3306)
+    crr = dtb.cursor()
+    crr.execute("""SELECT * FROM states WHERE name
+                LIKE '%N' ORDER BY states.id""")
+    dat = crr.fetchall()
+    for d in dat:
+        print(d)
+    crr.close()
+    dtb.close()
