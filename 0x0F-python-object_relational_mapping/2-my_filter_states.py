87eba420fb89dbd22ab616512059b6a18fb77214
Mohakad <mohkadtrd@gmail.com>
2024-03-16 18:25:15 +0300

    2filt

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""starting with N (upper N) from the database"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    dtb = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    crr = dtb.cursor()
+    crr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    dat = crr.fetchall()
+    for d in dat:
+        print(d)
+    crr.close()
+    dtb.close()
