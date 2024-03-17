f96cbee4f4deff5448836feff96ed8d90c92c30b
Mohakad <mohkadtrd@gmail.com>
2024-03-17 15:59:14 +0300

    model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class definition state"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+data = MetaData()
+Base = declarative_base(metadata=data)
+
+
+class State(Base):
+    """state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
