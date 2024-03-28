71b0c10163847b91c0d9cc20bcd0e3f687fbc786
Mohakad <mohkadtrd@gmail.com>
2024-03-28 21:49:35 +0300

    list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Find a peak"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    if not list_of_integers:
+        return None
+    
