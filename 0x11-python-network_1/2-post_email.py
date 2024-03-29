ac61d51eb0659457434cfdd4372f7eda036d8fe8
Mohakad <mohkadtrd@gmail.com>
2024-03-29 17:47:21 +0300

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,3 +4,9 @@
 
 import sys
 import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    ml = urllib.parse.urlencode({"email": sys.argv[2]}).encode('utf8')
+    with urllib.request.urlopen(sys.argv[1, ml]) as req:
+        print(req.read().decode('utf8'))
